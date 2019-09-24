f3c6bcb3af9f401f2bd4a9c96e9ee68b2bbe69ca
4joke <sniky_picky@tutanota.com>
2019-09-24 23:01:15 +0300

    добавлен поиск по категории

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
 import csv
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", help="file location", type=str)
-parser.add_argument("--comment", help="comment which we are looking for", nargs='+', required=True)
+parser.add_argument("--comment", help="comment which we are looking for", nargs='+')
+parser.add_argument("--category", help="category which we are looking for", nargs='+')
 args = parser.parse_args()
 
 
@@ -14,11 +16,17 @@ def calculate(file, comment):
         string+=word + " "
     with open(file, newline='', encoding='utf-8') as csvfile:
         scvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        if args.comment:
+            position = -1
+        elif args.category:
+            position = 2
         for row in scvreader:
-            if row[-1] == string.strip():
+            if row[position] == string.strip():
                 summa += float(row[3].replace("\"", ''))
     print(summa)
 
 
 if args.file and args.comment:
     calculate(args.file, args.comment)
+if args.file and args.category:
+    calculate(args.file, args.category)
